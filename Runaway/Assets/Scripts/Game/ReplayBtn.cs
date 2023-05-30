32f9397444d68b080cf372af073801462db30c3b
SE0NA <seona.git@gmail.com>
2023-05-30 17:14:45 +0900

    Game) Replay 버튼 광고 표시
    restPlay가 0이면 광고 이미지 버튼 설정

--- Runaway/Assets/Scripts/Game/ReplayBtn.cs
+++ Runaway/Assets/Scripts/Game/ReplayBtn.cs
@@ -9,6 +9,10 @@ public class ReplayBtn : MonoBehaviour
     [SerializeField] List<Image> list_img_heart;
     [SerializeField] Color color_rest;
 
+    [SerializeField] Image obj_btn_img;
+    [SerializeField] Sprite img_ad;
+    [SerializeField] Sprite img_replay;
+
     void Start()
     {
         SettingHeart();
@@ -38,6 +42,19 @@ public class ReplayBtn : MonoBehaviour
         for (int i = 0; i < DataManager.instance.restPlay; i++)
         {
             list_img_heart[i].color = color_rest;
+        }
+
+        RectTransform rt = obj_btn_img.GetComponent<RectTransform>();
+
+        if (DataManager.instance.restPlay <= 0) // 광고 버튼
+        {
+            rt.sizeDelta = new Vector2(rt.sizeDelta.x, 110);
+            obj_btn_img.sprite = img_ad;
+        }
+        else        // 리플레이 버튼
+        {
+            rt.sizeDelta = new Vector2(rt.sizeDelta.x, 140);
+            obj_btn_img.sprite = img_replay;
         }
     }
 }
